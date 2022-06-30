caa6212f40fb0af41423a31b7a6e42a4ee677811
amvincible <amvincible366@gmail.com>
2022-06-30 17:41:57 +0530

    start script added

--- controller/contactUsController.js
+++ controller/contactUsController.js
@@ -3,13 +3,21 @@ const {contactUs} = require("../models")
 const createContactUsCntrl = async(req,res) =>{
     const {Name, email, requirements , phone} = req.body
     let result = await contactUs.create({Name,email,requirements,phone})
-
+console.log("contactus");
     res.status(200).json({
         message: "created successfully",
         data: result
     })
 }
+const getData = async(req,res) =>{
+
+console.log("contactus");
+    res.status(200).json({
+        message: "created successfully",
+    })
+}
 
 module.exports = {
-    createContactUsCntrl
+    createContactUsCntrl,
+    getData
 }
--- routes/contactus.js
+++ routes/contactus.js
@@ -1,10 +1,13 @@
-const { createContactUsCntrl } = require("../controller/contactUsController");
+const { createContactUsCntrl, getData } = require("../controller/contactUsController");
 
 const router = require("express").Router();
 
 router 
     .route("/contact")
     .post(createContactUsCntrl)
+router 
+    .route("/contact")
+    .get(getData)
 
 
 
